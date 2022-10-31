b4a2050d6f2009c6396b16cbdf104aca9f8c1774
lily <1349607493@qq.com>
2022-10-31 14:57:35 +0800

    Fix missing check for hipEvent_t

--- src/hip_event.cpp
+++ src/hip_event.cpp
@@ -30,6 +30,19 @@ namespace hip {
 static amd::Monitor eventSetLock{"Guards global event set"};
 static std::unordered_set<hipEvent_t> eventSet;
 
+
+bool isValid(hipEvent_t event) {
+  // NULL event is always valid
+  if (event == nullptr) {
+    return true;
+  }
+  amd::ScopedLock lock(hip::eventSetLock);
+  if (hip::eventsSet.find(event) == hip::eventsSet.end()) {
+    return false;
+  }
+  return true;
+}
+
 bool Event::ready() {
   if (event_->status() != CL_COMPLETE) {
     event_->notifyCmdQueue();
@@ -355,6 +368,9 @@ hipError_t hipEventRecord_common(hipEvent_t event, hipStream_t stream) {
   if (event == nullptr) {
     return hipErrorInvalidHandle;
   }
+  if (!isValid(event)) {
+    return hipErrorContextIsDestroyed;
+  }
   hip::Event* e = reinterpret_cast<hip::Event*>(event);
   amd::HostQueue* queue = hip::getQueue(stream);
   if (g_devices[e->deviceId()]->devices()[0] != &queue->device()) {
@@ -380,7 +396,9 @@ hipError_t hipEventSynchronize(hipEvent_t event) {
   if (event == nullptr) {
     HIP_RETURN(hipErrorInvalidHandle);
   }
-
+  if (!isValid(event)) {
+    HIP_RETURN(hipErrorContextIsDestroyed);
+  }
   hip::Event* e = reinterpret_cast<hip::Event*>(event);
   HIP_RETURN(e->synchronize());
 }
@@ -389,7 +407,10 @@ hipError_t ihipEventQuery(hipEvent_t event) {
   if (event == nullptr) {
     return hipErrorInvalidHandle;
   }
-
+  if (!isValid(event)) {
+    HIP_RETURN(hipErrorContextIsDestroyed);
+  }
+  
   hip::Event* e = reinterpret_cast<hip::Event*>(event);
   return e->query();
 }
